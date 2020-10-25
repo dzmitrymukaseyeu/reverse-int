c60ca225e11dd250c1affb9f9789a340d6c0c4ad
Dzmitry Mukaseyeu <dimamukas@gmail.com>
2020-10-25 11:41:53 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let arr = [];
+  arr = String(n).split('');
+  (arr[0] == '-') ? arr = arr.slice(1, arr.length).reverse() : arr = arr.reverse(); 
+    return arr.join('');
 }
